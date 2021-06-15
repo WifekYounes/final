f3ef44fe88ce8f408484908dd8a0077665707f81
younes wifek <wifeky11@gmail.com>
2021-06-15 22:31:24 +0100

    flam

---
+++ src/app/models/constraint-co2.ts
@@ -0,0 +1,9 @@
+import { Device } from "./device";
+
+export class ConstraintCo2 {
+  idConstraint:number;
+  nameConstraint:String;
+    min_value:number;
+    max_value:number;
+    list_dev_constraints:Device[];
+}
